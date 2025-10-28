2033e322759ff3ea6c96bfd7ea2a78baf52d85fb
Emmanuel Lopez <emlopezdev@gmail.com>
2025-10-28 13:33:16 -0400

    Leads tracker app

---
+++ index.js
@@ -0,0 +1,54 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, push, onValue, remove } from "firebase/database";
+
+const firebaseConfig = {
+    databaseURL: import.meta.env.VITE_DATABASE_URL,
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const dbRef = ref(database, "leads");
+
+const inputEl = document.getElementById("input-el");
+const ulEl = document.getElementById("ul-el");
+const inputBtn = document.getElementById("input-btn");
+const deleteBtn = document.getElementById("delete-btn");
+
+function render(leads) {
+    let listItems = "";
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `;
+    }
+    ulEl.innerHTML = listItems;
+}
+
+onValue(dbRef, (snapshot) => {
+    if (snapshot.exists()) {
+        const values = snapshot.val();
+        const leads = Object.values(values);
+        render(leads);
+    }
+});
+
+deleteBtn.addEventListener("dblclick", function () {
+    remove(dbRef);
+    ulEl.innerHTML = "";
+});
+
+inputBtn.addEventListener("click", function () {
+    push(dbRef, inputEl.value);
+    inputEl.value = "";
+});
+
+inputEl.addEventListener("keydown", (evt) => {
+    if (evt.key === "Enter") {
+        push(dbRef, inputEl.value);
+        inputEl.value = "";
+    }
+});
